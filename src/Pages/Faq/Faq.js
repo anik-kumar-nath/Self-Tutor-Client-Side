af5cf7b5690b9cf9390e6a4e37d3f1fab3bae2f5
aknathweb <aniknwu.cse@gmail.com>
2022-10-26 22:46:16 +0600

    done FQA section work

--- src/Pages/Faq/Faq.js
+++ src/Pages/Faq/Faq.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Accordion } from 'react-bootstrap';
+import { useLoaderData } from 'react-router-dom';
 
 const Faq = () => {
+    const faqData = useLoaderData();
+
     return (
-        <div>
-            <h1>This is frequently asked question</h1>
+        <div className='container'>
+            <h1 className='text-center text-success mb-3 fw-bolder'>React.js interview frequently asked questions</h1>
+            {
+                faqData.map((data, index) =>
+                    <div className='m-3'>
+                        <Accordion defaultActiveKey="1">
+                            <Accordion.Item eventKey={data.id - 1}>
+                                <Accordion.Header>{data.Q}</Accordion.Header>
+                                <Accordion.Body>
+                                    {data.A}
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
+                    </div>)
+            }
+
         </div>
     );
 };
--- src/Routes/Route.js
+++ src/Routes/Route.js
@@ -18,6 +18,7 @@ export const routes = createBrowserRouter([
             },
             {
                 path: '/faq',
+                loader: () => fetch('FAQData.json'),
                 element: <Faq></Faq>
             },
             {
