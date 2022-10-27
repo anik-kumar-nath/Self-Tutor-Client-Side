fd0754964039967b5161a2a717e1ea53cbcc8626
aknathweb <aniknwu.cse@gmail.com>
2022-10-27 22:23:24 +0600

    some minor change

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Context/AuthContextProvider';
@@ -9,17 +8,22 @@ const Checkout = () => {
     console.log(checkout);
     return (
         <div>
-            <h1> This is checkout page {checkout.title}</h1>
-            <Card className='text-success'>
-                <Card.Header as="h5">Check Out</Card.Header>
-                <Card.Body>
-                    <Card.Title>{checkout.title}</Card.Title>
-                    <Card.Text>
-                        course ID: {checkout._id}
-                    </Card.Text>
-                    <Link to='/' className='btn btn-primary'>Go Course Page</Link>
-                </Card.Body>
-            </Card>
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 col-md-8 col-ld-6 mx-auto">
+                        <Card className='text-success'>
+                            <Card.Header as="h2">Check Out</Card.Header>
+                            <Card.Body>
+                                <Card.Title>{checkout.title}</Card.Title>
+                                <Card.Text>
+                                    Course ID: {checkout._id}
+                                </Card.Text>
+                                <Link to='/' className='btn btn-primary'>Go Course Page</Link>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
